9ddde3013f46c769497e9394c7fd37b647526da4
r0o0 <rayeoh7@gmail.com>
2019-07-26 16:22:00 +0900

    add utils for parsing string to HTML then to assemble html children nodes to one string data then make them all full words before render on screen

---
+++ client/src/utils/HTMLparser.js
@@ -0,0 +1,47 @@
+
+const HTMLparser = (content) => {
+  // console.log('html parser', content);
+  const parser = new DOMParser();
+  const toHTML = parser.parseFromString(content, 'text/html');
+  const parent = toHTML.querySelector('body');
+  const children = parent.childNodes;
+  let toAssemble = [];
+
+  for (let i = 0; i < children.length; i++) {
+    const textOnly = children[i].textContent;
+    console.log('ee', textOnly);
+    toAssemble.push(textOnly);
+  }
+
+  return toAssemble;
+};
+
+const checkFullWord = (toCheck) => {
+  let result;
+
+  const endsWithWord = new RegExp("\\b" + toCheck + "\\b").test(toCheck);
+  if (!endsWithWord) {
+    let newCheck;
+    for (let i = 0; i < toCheck.length; i++) {
+      newCheck = toCheck.slice(0, -i);
+      const endsWithWord = new RegExp("\\b" + newCheck + "\\b").test(newCheck);
+      if (endsWithWord) {
+        return newCheck;
+      }
+    }
+  } else {
+    result = toCheck;
+  }
+
+  return result;
+};
+
+export const transformToText = (content) => {
+  let result;
+  const toAssemble = HTMLparser(content);
+  const assembler = (acc, curr) => acc + ' ' + curr;
+  const assembled = toAssemble.reduce(assembler);
+  result = checkFullWord(assembled);
+
+  return result;
+}
