2188421221d97e263a240826307b160c3d754f91
r0o0 <rayeoh7@gmail.com>
2019-07-26 16:23:03 +0900

    add a preview option when rendering Article component

--- client/src/components/Article/index.tsx
+++ client/src/components/Article/index.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import * as type from '../../types';
 // UTILS
 import { getCleaned } from '../../utils/sanitizeHTML';
-
+import { transformToText } from '../../utils/HTMLparser';
 interface Props {
   title: string;
   content: string;
+  preview?: boolean;
 }
 
 function Article(props: Props) {
-  const { title, content } = props;
-  const cleanContent = getCleaned(content);
+  const { title, content, preview } = props;
+  let cleanContent;
+  if (preview) {
+    const readyToParse = content.substr(0, 150);
+    cleanContent = transformToText(readyToParse) as string;
+  } else {
+    cleanContent = getCleaned(content);
+  }
+
   return (
     <div>
       <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{__html: cleanContent}}></div>
+      {preview ?
+        <p dangerouslySetInnerHTML={{__html: cleanContent}}></p> :
+        <div dangerouslySetInnerHTML={{__html: cleanContent}}></div>
+      }
     </div>
   );
 }
