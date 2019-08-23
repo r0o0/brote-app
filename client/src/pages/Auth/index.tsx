cae6ce52a36771ed8274cd3ad9105a470529f826
r0o0 <rayeoh7@gmail.com>
2019-08-24 00:15:47 +0900

    fix updated Auth component directory

--- client/src/pages/Auth/index.tsx
+++ client/src/pages/Auth/index.tsx
@@ -8,7 +8,7 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogActions from '@material-ui/core/DialogActions';
-import Auth from '../../components/Auth/Auth';
+import Auth from '../../components/Auth';
 // CSS
 import { LoginStyles } from './AuthStyles';
 
