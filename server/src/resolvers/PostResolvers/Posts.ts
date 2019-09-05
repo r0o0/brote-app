87b48dd9c615e7e799fcffb01162b010e492954d
r0o0 <rayeoh7@gmail.com>
2019-09-05 11:30:19 +0900

    add count of posts data: total, drafts, published

--- server/src/resolvers/PostResolvers/Posts.ts
+++ server/src/resolvers/PostResolvers/Posts.ts
@@ -1,7 +1,52 @@
+import {
+  checkForUser,
+  checkForGuest,
+} from '../UserResolvers/userUtils';
+
 const Posts = {
   // query
-  posts(_, args, { db }) {
-    return db.query.posts();
+  async posts(_, args, { db, request }) {
+    const user = request.user;
+    if(!user) throw Error('Not authenticated');
+    const totalConnection = await db.query.postsConnection({
+      where: {
+        author: {
+          email: user.email
+        }
+      }
+    }, `{ aggregate { count } }`);
+    const total = totalConnection.aggregate.count;
+    const draftConnection = await db.query.postsConnection({
+      where: {
+        author: {
+          email: user.email
+        },
+        isPublished: false,
+      }
+    }, `{ aggregate { count } }`);
+    const drafts = draftConnection.aggregate.count;
+    const publishedConnection = await db.query.postsConnection({
+      where: {
+        author: {
+          email: user.email
+        },
+        isPublished: true,
+      }
+    }, `{ aggregate { count } }`);
+    const published = publishedConnection.aggregate.count;
+    const data = await db.query.posts({
+      where: {
+        author: {
+          email: user.email
+        }
+      }
+    });
+    return {
+      total,
+      drafts,
+      published,
+      data
+    };
   },
   publishedPosts(_, args, { db }) {
     return db.query.posts({
@@ -10,6 +55,25 @@ const Posts = {
       }
     });
   },
+  myDrafts(_, args, { db, request }) {
+    const user = request.user;
+    if(!user) throw Error('Not authenticated');
+
+    let author;
+    if(user.email) {
+      const isUser = checkForUser(db, user.email);
+      if (isUser) author = user.email;
+    }
+    if (user.name && !user.email) {
+      const isGuest = checkForUser(db, user.name);
+      if (isGuest) author = user.name;
+    }
+    return db.query.posts({
+      where: {
+        author,
+      }
+    })
+  }
 };
 
 export default Posts;
