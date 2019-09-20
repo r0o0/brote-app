1aa3ba29c55b3a6705ae53cc163b675615907889
r0o0 <rayeoh7@gmail.com>
2019-09-21 00:50:47 +0900

    change access origin to allow all domains

--- server/src/index.ts
+++ server/src/index.ts
@@ -66,7 +66,7 @@ server.get("/posts", (req, res) => {
 });
 
 server.express.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_ENDPOINT);
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
